90dea339442b82bc0c083277029fc0f261b08327
Jeanll7 <jeancarloleall@gmail.com>
2024-08-16 10:04:09 -0300

    feat: add ChartOverview component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,95 @@
-import { Button } from "@/components/ui/button";
+import ChartOverview from "@/components/chart-overview";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { BadgeDollarSign, DollarSign, Percent, Users } from "lucide-react";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-2xl font-semibold">Projeto Dashboard</h1>
+    <main className="sm:ml-14 p-4">
+      <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> 
+        {/* CARD 1 */}
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Total vendas
+              </CardTitle>
+              <DollarSign className="ml-auto w-4 h-4" />
+            </div>
+
+            <CardDescription>
+              Total vendas em 90 dias
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">R$ 40.000</p>
+          </CardContent>
+        </Card>
+
+        {/* CARD 2 */}
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Novos clientes
+              </CardTitle>
+              <Users className="ml-auto w-4 h-4" />
+            </div>
+
+            <CardDescription>
+              Novos clientes em 30 dias
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">234</p>
+          </CardContent>
+        </Card>
+
+        {/* CARD 3 */}
+        <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Pedidos hoje
+              </CardTitle>
+              <Percent className="ml-auto w-4 h-4" />
+            </div>
+
+            <CardDescription>
+              Total de pedidos hoje
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">65</p>
+          </CardContent>
+        </Card>
+
+         {/* CARD 4 */}
+         <Card>
+          <CardHeader>
+            <div className="flex items-center justify-center">
+              <CardTitle className="text-lg sm:text-xl text-gray-800 select-none">
+                Total pedidos
+              </CardTitle>
+              <BadgeDollarSign className="ml-auto w-4 h-4" />
+            </div>
+
+            <CardDescription>
+              Total de pedidos em 30 dias
+            </CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p className="text-base sm:text-lg font-bold">2300</p>
+          </CardContent>
+        </Card>
+      </section>
+
+      <section>
+        <ChartOverview />
+      </section>
     </main>
   );
 }
---
+++ src/components/chart-overview.tsx
@@ -0,0 +1,9 @@
+const ChartOverview = () => {
+  return (
+    <div>
+      <h2>Grafico</h2>
+    </div>
+  );
+}
+ 
+export default ChartOverview;
